1f6d3caacff72350a7fbc7e880a9e07d3eece807
ainaelyani <147725313+ainaelyani@users.noreply.github.com>
2025-03-12 15:25:26 +0800

    Update header.php
    change the color

--- layout/header.php
+++ layout/header.php
@@ -16,7 +16,7 @@
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand" style="background-color: #064635 ;">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="/inventory-system/dashboard.php">Inventory System</a>
+        <a class="navbar-brand ps-3" style= "color:white;" href="/inventory-system/dashboard.php">Inventory System</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
         <!-- Navbar Search-->
@@ -26,7 +26,7 @@
         <!-- Navbar-->
         <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" style= "color:white;" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="/inventory-system/setting.php">Settings</a></li>
                     <li>
